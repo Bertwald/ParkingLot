05e20f1712005264d8149a0cc2bf745118059904
Bertwald <thomas_ace8@hotmail.com>
2022-11-13 20:11:17 +0100

    A smarter kind of stupid as it is hard to define what constitutes intelligent placement

--- ParkingLot/Logic/ParkingGarage.cs
+++ ParkingLot/Logic/ParkingGarage.cs
@@ -1,6 +1,5 @@
 ﻿using ParkingDeluxe.UserInterface;
 using ParkingDeluxe.Vehicles;
-using System.Drawing;
 
 namespace ParkingDeluxe.Logic {
     internal enum InputMode {
@@ -21,32 +20,77 @@ namespace ParkingDeluxe.Logic {
         private static readonly int s_scalingFactor = 2;
         private readonly int _numberOfSpaces;
         private readonly bool[] _isOccupied;
-        private readonly Dictionary<Vehicle, int> _parkedVehicles;
+        private readonly Dictionary<Vehicle, int> _parkedVehiclesToParkingNumber;
+        private readonly Dictionary<int, int> _ParkingNumberToFreeSpace;
         private readonly ParkingQueue _queue;
         private InputMode _inputMode;
 
         internal ParkingGarage(int slots, ParkingQueue queue) {
             _numberOfSpaces = slots * s_scalingFactor;
             _isOccupied = new bool[_numberOfSpaces];
-            _parkedVehicles = new Dictionary<Vehicle, int>();
+            _parkedVehiclesToParkingNumber = new Dictionary<Vehicle, int>();
+            _ParkingNumberToFreeSpace = new();
             _queue = queue;
             _inputMode = InputMode.Manual;
         }
+        /**
+         *  The "intelligence" entails two simple steps
+         *  1: Try to avoid creating a mess in the first place, by packing things as tight as possible
+         *  2: As time goes on, try to keep it as close to how it initially were, by trying to place new vehicles where similar sized vehicles were placed
+         *  2a: The bus slots will be avoided after a bus has unparked for as long as smaller vehicles has previously been parked elsewhere
+         *  
+         *  Possible Improvements that could be made without a major change in datastructures and overall approach
+         *  Merge slots for MCs so that larger vehicles can use these slots too, add "suitable" slots which has not previously been occupied 
+         *  whenever a smaller vehicle first breaks a bus-slot (can easily be calculated by index)
+         *  As Time goes on, a mess WILL be created
+         * 
+         */
         private bool ParkVehicle(Vehicle vehicle) {
+            //First we check if any vehicle has unparked recently so that an index is free, only for small vehicles
+            if (ExecutedParkingAtFreedSpace(vehicle)) {
+                return true;
+            }
             //Due to parking layout we cannot park the bus at all consequent spaces, and even if we could that would result in non optimal bus placement
             //The number of spaces need to be an even number for "optimal behavior"
             for (int parkingNumber = 0; parkingNumber < _isOccupied.Length; parkingNumber += vehicle.Size) {
                 if (IsWithinParking(parkingNumber, vehicle.Size) && CanVehicleFit(parkingNumber, vehicle.Size)) {
-                        _parkedVehicles.Add(vehicle, parkingNumber);
-                        vehicle.StartParkingTimer();
-                        vehicle.ParkedInInterval = GetParkingInterval(vehicle.Size, parkingNumber);
-                        SetVehicleOccupation(vehicle, true);
-                        return true;
+                    ParkVehicleAt(vehicle, parkingNumber);
+                    RemoveFreedSpaces(parkingNumber, vehicle.Size);
+                    return true;
                 }
             }
             return false;
         }
-
+        private bool ExecutedParkingAtFreedSpace(Vehicle vehicle) {
+            if (vehicle is Motorcycle || vehicle is Car) {
+                var freeSpaces = from space in _ParkingNumberToFreeSpace
+                                 where space.Value >= vehicle.Size
+                                 orderby space.Value ascending
+                                 select space;
+                if (freeSpaces is not null && freeSpaces.Any()) {
+                    foreach (KeyValuePair<int, int> space in freeSpaces) {
+                        if (vehicle is Motorcycle && vehicle.Size < space.Value) {
+                            _ParkingNumberToFreeSpace.Add(space.Key + vehicle.Size, space.Value - vehicle.Size);
+                        }
+                        ParkVehicleAt(vehicle, space.Key);
+                        RemoveFreedSpaces(space.Key, 1);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void ParkVehicleAt(Vehicle vehicle, int parkingNumber) {
+            _parkedVehiclesToParkingNumber.Add(vehicle, parkingNumber);
+            vehicle.StartParkingTimer();
+            vehicle.ParkedInInterval = GetParkingInterval(vehicle.Size, parkingNumber);
+            SetVehicleOccupation(vehicle, true);
+        }
+        private void RemoveFreedSpaces(int parkingNumber, int size) {
+            for (int i = 0; i < size; i++) {
+                _ParkingNumberToFreeSpace.Remove(parkingNumber + i);
+            }
+        }
         private bool CanVehicleFit(int parkingNumber, int size) {
             for (int j = 0; j < size; j++) {
                 if (_isOccupied[parkingNumber + j]) {
@@ -64,21 +108,23 @@ namespace ParkingDeluxe.Logic {
         }
         internal void UnparkVehicle(Vehicle vehicle) {
             SetVehicleOccupation(vehicle, false);
+            if (vehicle.Size < 4) {
+                    _ParkingNumberToFreeSpace.Add(_parkedVehiclesToParkingNumber[vehicle], vehicle.Size);
+            }
             RemoveVehicleFromDatabase(vehicle);
             double fee = GetParkingFee(vehicle);
             UI.PrintUnParkingInfo(vehicle, fee);
         }
-
         private void SetVehicleOccupation(Vehicle vehicle, bool occupation) {
-            int startindex = _parkedVehicles[vehicle];
+            int startindex = _parkedVehiclesToParkingNumber[vehicle];
             for (int i = 0; i < vehicle.Size; i++) {
                 _isOccupied[startindex + i] = occupation;
             }
         }
         private void RemoveVehicleFromDatabase(Vehicle vehicle) {
             // If we cannot remove the vehicle, we need to check if it is still in the dictionary/database
-            if (!_parkedVehicles.Remove(vehicle)) {
-                if (_parkedVehicles.ContainsKey(vehicle)) {
+            if (!_parkedVehiclesToParkingNumber.Remove(vehicle)) {
+                if (_parkedVehiclesToParkingNumber.ContainsKey(vehicle)) {
                     UI.PrintParkingError("Could not remove vehicle entry from database, contact technical support", false);
                 }
             }
@@ -87,33 +133,29 @@ namespace ParkingDeluxe.Logic {
             return s_spaceBasedFee ? vehicle.GetParkedTimeInMinutes() * s_parkingFee * vehicle.Size / 2 :
                                      vehicle.GetParkedTimeInMinutes() * s_parkingFee;
         }
-
         internal void ListParkedCars() {
-            var vehicles = from vehicle in _parkedVehicles
+            var vehicles = from vehicle in _parkedVehiclesToParkingNumber
                            orderby vehicle.Value
                            select vehicle.Key;
             UI.PrintParking<Vehicle>(vehicles);
         }
-
         internal void Run() {
             bool continueRunning;
             do {
                 Console.Clear();
                 ListParkedCars();
                 if (IsParkingFull()) {
-                    UI.FullParkingNotification(_parkedVehicles.Count);
+                    UI.FullParkingNotification(_parkedVehiclesToParkingNumber.Count);
                 }
-                UI.ShowMenu(_parkedVehicles.Count + 2);
+                UI.ShowMenu(_parkedVehiclesToParkingNumber.Count + 2);
                 PrintInputMode();
                 Command command = InputModule.GetCommand();
                 continueRunning = ProcessCommand(command);
             } while (continueRunning);
         }
-
         private void PrintInputMode() {
             Console.WriteLine("Aktivt inmatningsläge : " + _inputMode + Environment.NewLine);
         }
-
         private bool ProcessCommand(Command command) => command switch {
             Command.Park => InitiateParkingProcess(),
             Command.Unpark => InitiateUnparkingProcess(),
@@ -122,7 +164,6 @@ namespace ParkingDeluxe.Logic {
             Command.DoNothing => true,
             _ => true,
         };
-
         private bool ToggleInputMode() {
             _inputMode = (InputMode)((int)(_inputMode + 1) % (int)InputMode.MAX);
             return true;
@@ -135,11 +176,10 @@ namespace ParkingDeluxe.Logic {
             }
             return true;
         }
-
         private bool InitiateUnparkingProcess() {
             UI.ShowUnparkingInstructions();
             string licenceNumber = InputModule.GetString();
-            var vehicles = from vehicle in _parkedVehicles
+            var vehicles = from vehicle in _parkedVehiclesToParkingNumber
                            where vehicle.Key.LicenseNumber == licenceNumber
                            select vehicle.Key;
             if (vehicles is null || !vehicles.Any()) {
